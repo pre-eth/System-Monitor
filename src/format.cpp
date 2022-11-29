dbe56e093038aeb77bbf6e889dc83e2c1b8bbfee
Preeth Vijay <preethvijay0@gmail.com>
2022-11-28 19:21:12 -0500

    - add return code to main.cpp - condense some formatting in system.cpp - implement body of helper function to format time in format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,23 @@
 #include <string>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    std::ostringstream date;
+
+    int hours = std::floor(seconds / 3600);
+    seconds %= 3600;
+    int min = std::floor(seconds / 60);
+    seconds %= 60;
+    
+    date << std::setw(2) << std::setfill('0') << hours;
+    date << ":" << std::setw(2) << min;
+    date << ":" << seconds;
+
+    return date.str();
+ }
